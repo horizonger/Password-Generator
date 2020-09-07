7b270a955653acc17d29cd9bada400dcd0dc845b
ufuk <70153893+horizonger@users.noreply.github.com>
2020-09-07 16:44:02 +0300

    Update V0.1 pass.py

--- V0.1 pass.py
+++ V0.1 pass.py
@@ -1,34 +1,38 @@
-#This version writes data to txt file.
+# This version writes data to txt file.
+# modules
 import string
 import random
 
 
+# file process part
 def file(out_file):
-    with open("passwords.txt", "a") as pass_file:
-        pass_file.write("\n")
-        pass_file.writelines(out_file)
-        pass_file.close()
+    with open("passwords.txt", "a") as password_file:
+        password_file.write("\n")
+        password_file.writelines(out_file)
+        password_file.close()
 
 
+# Variables
 pass_char0 = string.punctuation + string.ascii_letters + string.digits
 pass_char1 = string.ascii_lowercase + string.ascii_uppercase
 pass_char = pass_char0 + pass_char1
 
+# user input part
 while True:
-    pass_len = input("Choose password length: ")
-
-    if pass_len.isdigit():
-        new_v = []
-        for cycle in range(int(pass_len)):
-            pass_gen = random.choice(pass_char)
-            print(pass_gen, end="")
-            new_v.append(pass_gen)
+    User_input = input("Choose password length: ")
+    # this part create password character when input value is true
+    if User_input.isdigit():
+        File_variable = []
+        for password_loop in range(int(User_input)):
+            password_generator_variable = random.choice(pass_char)
+            print(password_generator_variable, end="")
+            File_variable.append(password_generator_variable)
         print(" ")
-        file(new_v)
-    elif not pass_len.isdigit():
+        file(File_variable)
+    # this part writes incorrectly entered values to the screen
+    elif not User_input.isdigit():
         wrong_input = []
-        for char in pass_len:
-            if char not in string.digits:
-                wrong_input.append(char)
-        print(f"I need integer value and you entered this value {wrong_input}")
-        print(wrong_input)
+        for wrong_input_loop in User_input:
+            if wrong_input_loop not in string.digits:
+                wrong_input.append(wrong_input_loop)
+        print(f"I need integer value and you entered this value(s) {wrong_input}")
