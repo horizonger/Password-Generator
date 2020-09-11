e07c53a23e7f60d5ab37fd1b1a97ed3d193ad7e5
ufuk <70153893+horizonger@users.noreply.github.com>
2020-09-11 17:30:50 +0300

    Update V0.3 pass.py

--- V0.3 pass.py
+++ V0.3 pass.py
@@ -1,30 +1,33 @@
+# This version writes data to txt file.
+# modules
 import string
 import random
 
-
+#write to file part
 def file_write(out_file):
     with open("passwords.txt", "a") as pass_file:
         pass_file.write("\n")
         pass_file.writelines(out_file)
 
-
+#read to file part
 def file_read():
     with open("passwords.txt", "r") as file_written:
         for item in file_written:
             print(item, end=" ")
 
-
+## Variables
 pass_char0 = string.punctuation + string.ascii_letters + string.digits
 pass_char1 = string.ascii_lowercase + string.ascii_uppercase
 pass_char = pass_char0 + pass_char1
 
+# user input part
 while True:
     pass_len = input("""
 (IF YOU WANNA SEE OLD PASSWORDS JUST WRITE OLD PASSWORD) 
 ENTER THE PASSWORD LENGTH
 >>>>
 """)
-
+   # this part create password character when input value is true
     if pass_len.isdigit():
         new_v = []
         for cycle in range(int(pass_len)):
@@ -33,8 +36,10 @@ ENTER THE PASSWORD LENGTH
             new_v.append(pass_gen)
         print(" ")
         file_write(new_v)
+    # this part for the read old data
     elif pass_len == "old password".lower():
         file_read()
+    # this part writes incorrectly entered values to the screen
     elif not pass_len.isdigit():
         wrong_input = []
         for char in pass_len:
