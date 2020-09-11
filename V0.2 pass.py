5448a5d096137ba8781885ab7aa7e92648d8b2a2
ufuk <70153893+horizonger@users.noreply.github.com>
2020-09-11 21:59:33 +0300

    Update and rename V0.2 pass.py . to V0.2 pass.py

--- V0.2 pass.py
+++ V0.2 pass.py
@@ -1,8 +1,9 @@
+#this version writes data to excel file
 import string
 import random
 import xlsxwriter
 
-
+#write to excel file part
 def file(out_file):
     out_x = xlsxwriter.Workbook("password.xlsx")
     pass_w = out_x.add_worksheet()
@@ -10,13 +11,15 @@ def file(out_file):
     pass_w.write("A2", str(out_file))
     out_x.close()
 
-
+# Variables
 pass_char0 = string.punctuation + string.ascii_letters + string.digits
 pass_char1 = string.ascii_lowercase + string.ascii_uppercase
 pass_char = pass_char0 + pass_char1
 
+# user input 
 pass_len = input("Choose password length: ")
 
+# this part create password character when input value is true
 if pass_len.isdigit():
     new_v = []
     for cycle in range(int(pass_len)):
@@ -25,7 +28,9 @@ if pass_len.isdigit():
         new_v.append(pass_gen)
     print(" ")
     file(new_v)
-elif not pass_len.isdigit():
+ 
+ # this part writes incorrectly entered values to the screen
+ elif not pass_len.isdigit():
     wrong_input = []
     for char in pass_len:
         if char not in string.digits:
